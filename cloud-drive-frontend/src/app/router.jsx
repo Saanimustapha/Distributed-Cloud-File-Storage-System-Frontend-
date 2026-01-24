448321e56170481c0b39837dc796de3791b9d422
Saani Mustapha Deishini <deishini2003@gmail.com>
2026-01-24 20:55:09 +0000

    Added the drive landing page.

--- cloud-drive-frontend/src/app/router.jsx
+++ cloud-drive-frontend/src/app/router.jsx
@@ -3,7 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import { ProtectedRoute } from "../components/layout/ProtectedRoute";
 import LoginPage from "../features/auth/LoginPage";
 import RegisterPage from "../features/auth/RegisterPage";
-// import DrivePage from "../features/drive/DrivePage";
+import DrivePage from "../features/drive/DrivePage";
 // import FileDetailsPage from "../features/files/FileDetailsPage";
 // import SharedPage from "../features/shared/SharedPage";
 // import NodesPage from "../features/admin/nodes/NodesPage";
@@ -13,20 +13,21 @@ export const router = createBrowserRouter([
   { path: "/login", element: <LoginPage /> },
   { path: "/register", element: <RegisterPage /> },
 
-//   {
-//     path: "/app",
-//     element: (
-//       <ProtectedRoute>
-//         <AppShell />
-//       </ProtectedRoute>
-//     ),
-//     children: [
-//       { path: "drive", element: <DrivePage /> },
-//       { path: "shared", element: <SharedPage /> },
-//       { path: "files/:fileId", element: <FileDetailsPage /> },
-//       { path: "admin/nodes", element: <NodesPage /> },
-//     ],
-//   },
+  {
+    path: "/app",
+    element: (
+      // <ProtectedRoute>
+      //   <AppShell />
+      // </ProtectedRoute>
+      <AppShell />
+    ),
+    children: [
+      { path: "drive", element: <DrivePage /> },
+      // { path: "shared", element: <SharedPage /> },
+      // { path: "files/:fileId", element: <FileDetailsPage /> },
+      // { path: "admin/nodes", element: <NodesPage /> },
+    ],
+  },
 
   { path: "*", element: <LoginPage /> },
 ]);
